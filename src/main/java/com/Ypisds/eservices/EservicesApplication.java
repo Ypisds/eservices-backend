d8549e0474ed8f67bcf1bfaa11a15d3dff8b7bbf
Ypisds <thrbritto4@gmail.com>
2026-01-09 15:23:15 -0300

    first commit

---
+++ src/main/java/com/Ypisds/eservices/EservicesApplication.java
@@ -0,0 +1,13 @@
+package com.Ypisds.eservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ypisds/eservices/EservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ypisds.eservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
